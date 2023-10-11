500c4f515ee05b0446bb9b9a306094819b4ca440
kappakkala <thto1013@gmail.com>
2023-10-11 17:25:30 +0200

    output file, formatting, string intro

--- TypeScriptDemos/README.md
+++ TypeScriptDemos/README.md
@@ -2,7 +2,7 @@
 
 ## Installation and set up
 
-Install **node.js**
+Install **node.js**.
 
 Create a new *package.json* file using
 ```
@@ -14,7 +14,6 @@ npm i typescript
 ```
 Update npm if necessary `npm install -g npm@10.2.0`. Create a **tsconfig** file `tsc --init`. If it results an error, then try [installing typescript](https://stackoverflow.com/a/36917406) using 
 `npm install -g typescript`. Check version using `tsc -v`. 
-
 ```
 tsc --init. 
 ```
@@ -25,8 +24,11 @@ The *module* parameter inside the generated *tsconfig.json* decides where to run
 Create a typescript file *datatypes.ts*.
 
 Compile the code using `tsc`. This will create a new file *datatypes.js*, which is similar to the ts file. To run the code enter `node datatypes`. Once more changes has been made to the *datatypes.js* file, execute it by running
-
 ```
 tsc
 node datatypes
+```
+To write all the [console output into a text file](https://stackoverflow.com/a/69091148) *datatypes_output.txt*, run 
+```
+node datatypes > datatypes_output.txt 2>&1
 ```
--- TypeScriptDemos/datatypes.ts
+++ TypeScriptDemos/datatypes.ts
@@ -12,6 +12,184 @@ let strObj = new String("test");
 console.log(strObj);
 //--------------------------------------------------------------------------------------------------
 
+//--------------------------------------------------------------------------------------------------
+// List of Methods
+//--------------------------------------------------------------------------------------------------
+// 1 Conversion
+//// toString() 	        Returns a string or a string object as a string
+//// toLowerCase() 	        Returns a string converted to lowercase letters
+//// toUpperCase() 	        Returns a string converted to uppercase letters
+//// toLocaleLowerCase() 	Returns a string converted to lowercase letters, using the host's locale
+//// toLocaleUpperCase() 	Returns a string converted to uppercase letters, using the host's locale
+//// fromCharCode() 	    Returns Unicode values as characters
+
+// 2 Index based
+//// charAt()   	        Returns the character at a specified index (position)
+//// charCodeAt() 	        Returns the Unicode of the character at a specified index
+//// indexOf() 	            Returns the index (position) of the first occurrence of a value in a string
+//// lastIndexOf() 	        Returns the index (position) of the last occurrence of a value in a string
+
+// 3 Value based
+//// search()   	        Searches a string for a value, or regular expression, and returns the index (position) of the match
+//// match() 	            Searches a string for a value, or a regular expression, and returns the matches
+//// includes() 	        Returns if a string contains a specified value
+//// valueOf()  	        Returns the primitive value of a string or a string object
+//// repeat() 	            Returns a new string with a number of copies of a string
+//// replace() 	            Searches a string for a pattern, and returns a string where the first match is replaced
+//// replaceAll()        	Searches a string for a pattern and returns a new string where all matches are replaced
+//// startsWith() 	        Checks whether a string begins with specified characters
+//// endsWith() 	        Returns if a string ends with a specified value
+//// localeCompare()    	Compares two strings in the current locale
+
+// 4 Extraction
+//// slice() 	            Extracts a part of a string and returns a new string
+//// substring()            Extracts characters from a string, between two specified indices (positions) 
+//// substr() 	            Extracts a number of characters from a string, from a start index (position)
+//// trim() 	            Returns a string with removed whitespaces
+//// trimEnd() 	            Returns a string with removed whitespaces from the end
+//// trimStart() 	        Returns a string with removed whitespaces from the start
+//// split() 	            Splits a string into an array of substrings
+//// concat() 	            Returns two or more joined strings
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+// List of Properties
+//--------------------------------------------------------------------------------------------------
+//// length 	            Returns the length of a string
+//// constructor   	        Returns the string's constructor function
+//// prototype    	        Allows you to add properties and methods to an object
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+// 1_1 toString()
+// converts the datatype to string
+let str_num = 1234;
+let str_city : string;
+str_city =  " El Dorado "
+console.log("toString() 1234 : " + str_num.toString());
+
+// 1_2 toLowerCase()     
+// converts string to lowercase without changing the original value in the variable
+console.log("toLowerCase() '"+ str_city + "' : " + str_city.toLowerCase());
+
+// 1_3 toUpperCase()	        
+// converts string to uppercase without changing the original value in the variable
+console.log("toUpperCase() '"+ str_city + "' : " + str_city.toUpperCase());
+
+// 1_4 toLocaleLowerCase()	
+// converts string to lowercase using current locale, without changing the original value in the variable
+console.log("toLocaleLowerCase() '"+ str_city + "' : " + str_city.toLocaleLowerCase());
+
+// 1_5 toLocaleUpperCase()
+// converts string to uppercase using current locale, without changing the original value in the variable
+console.log("toLocaleupperCase() '"+ str_city + "' : " + str_city.toLocaleUpperCase());
+
+// 1_6 fromCharCode()
+// converts unicode to string characters
+console.log("fromCharCode() single character from unicode 73 : ", String.fromCharCode(73));
+console.log("fromCharCode() text from unicode 68,79,71 : ", String.fromCharCode(68,79,71));
+
+// 2_1 charAt()
+str_city =  "mississippi"
+// get the character at an index in a string
+console.log("charAt() index 4 in '" + str_city + "' : ", str_city.charAt(4));
+
+// 2_2 charCodeAt()	    
+// get the unicode value of a character at an index in a string
+console.log("charCodeAt() index 4 in '" + str_city + "' : ", str_city.charCodeAt(4));
+
+// 2_3 indexOf()
+// get the first index of the first occurence of a value in a string
+console.log("indexOf() 'i' in '" + str_city + "' : ", str_city.indexOf('i'));
+
+// 2_4 lastIndexOf()
+// get the last index of the first occurence of a value in a string
+console.log("lastIndexOf() 'i' in '" + str_city + "' : ", str_city.lastIndexOf('i'));
+
+// 3_1 search()   	      
+// matches the string against a regular expression and returns the index of the first occurence of a value, else -1
+// case sensitive
+console.log("search() 'o' in '" + str_city + "' : ", str_city.search('o'));
+
+// 3_2 match() 	           
+// matches the string against a regular expression and returns an array that contains info of the first occurence of a value, else null
+console.log("match() 'ss' in '" + str_city + "' : ", str_city.match('ss'));
+console.log("match() 'os' in '" + str_city + "' : ", str_city.match('os'));
+
+// 3_3 includes() 	        
+// returns true if the string contains a specific value, else false
+console.log("includes() 'os' in '" + str_city + "' : ", str_city.includes('os'));
+
+// 3_4 valueOf()  	        
+//
+console.log();
+
+// 3_5 repeat() 	           
+//
+console.log();
+
+// 3_6 replace() 	            
+//
+console.log();
+
+// 3_7 replaceAll()        
+//
+console.log();
+
+// 3_8 startsWith() 	       
+//
+console.log();
+
+// 3_9 endsWith() 	       
+//
+console.log();
+
+// 3_10 localeCompare()    	
+//
+console.log();
+
+// 4_1 slice() 	            
+//
+console.log();
+
+// 4_2 substring()            
+//
+console.log();
+
+// 4_3 substr() 	        
+//
+console.log();
+
+// 4_4 trim() 	            
+//
+console.log();
+
+// 4_5 trimEnd() 	        
+//
+console.log();
+
+// 4_6 trimStart() 	        
+//
+console.log();
+
+// 4_7 split() 	            
+//
+console.log();
+
+// 4_8 concat() 
+// 
+console.log();
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+// length 	            
+
+// constructor   	     
+
+// prototype    	     
+
+//--------------------------------------------------------------------------------------------------
+
 
 
 //##################################################################################################//
@@ -57,26 +235,26 @@ console.log(boolObj);
 //--------------------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------------------
-// 1 BOOLEAN()
+// 1 Boolean()
 // Checks for boolean datatype
 // Boolean(condition) or (condition)
 console.log("Boolean() 5>6? : " + Boolean(5>6));
 
-// 2 TOSTRING()
+// 2 ToString()
 // converts a boolean value to string
 console.log("toString() true : " + isValid.toString());
 
-// 3 VALUEOF()
+// 3 valueOf()
 // returns the value of a boolean variable
 console.log("valueOf() isValid : " + isValid.valueOf());
 //--------------------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------------------
-// CONSTRUCTOR
+// constructor
 // the property returns the function that created the boolean prototype
 console.log("constructor for true : " + true.constructor); 
 
-// PROTOTYPE
+// prototype
 // used to create a new method for a boolean
 declare global {
     interface Boolean {
@@ -158,7 +336,7 @@ console.log(arrObj);
 //// slice() 	    Selects a part of an array, and returns the new array
 //// splice() 	    Adds/Removes elements from an array
 //// reduce() 	    Reduce the values of an array to a single value (going left-to-right)
-///// reduceRight() Reduce the values of an array to a single value (going right-to-left)
+//// reduceRight()  Reduce the values of an array to a single value (going right-to-left)
 
 //--------------------------------------------------------------------------------------------------
 // List of Properties
@@ -169,26 +347,26 @@ console.log(arrObj);
 //--------------------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------------------
-// 1_1 ISARRAY()
+// 1_1 isArray()
 // returns true if an object is an array. 
 //  Using x.isArray(), where x is an array will return undefined.
 console.log("isArray() ['a','b'] : ", Array.isArray(["a","b"]));
 console.log("isArray() 'ab' : ", Array.isArray("ab"));
 
-// 1_2 TOSTRING()
+// 1_2 toString()
 // convert array to comma separated string values
 console.log("toString() : "  + empList.toString());
 
-// 1_3 FROM()
+// 1_3 from()
 // returns an array from an iterable object
 console.log("from() test : ", Array.from("test"));
 
-// 2_1 AT()
+// 2_1 at()
 // returns array element at a position
 // default 0. -1 returns the last element
 console.log("at() 3rd element in " + numList + " : " + numList.at(2));
 
-// 2_2 KEYS()
+// 2_2 keys()
 // returns an array iterator object that contains the keys of the array
 function getKeys(arr : any) {
     let arr_res = new Array; 
@@ -199,7 +377,7 @@ function getKeys(arr : any) {
 }
 console.log("keys() of ['a','b','c','d','e'] : ", getKeys(["a","b","c","d","e"]));
 
-// 2_3 FINDINDEX()
+// 2_3 findIndex()
 // returns the index of the first element in an array, that passes a certain test
 // returns -1 if no match is found
 function isEqual(num : any){
@@ -207,36 +385,36 @@ function isEqual(num : any){
 }
 console.log("findIndex() 7 appears first in [1,2,7,7,3] at index : " + [1,2,7,7,3].findIndex(isEqual));
 
-// 2_4 INDEXOF()
+// 2_4 indexOf()
 // returns first index of an element starting from particular position (default 0) while searching from left to right
 // return -1 if not found
 console.log("indexOf() 3 in [1,2,3,4,5,3] from index 0: ", [1,2,3,4,5,3].indexOf(3));
 console.log("indexOf() 3 in [1,2,3,4,5,3] from index 3 : ", [1,2,3,4,5,3].indexOf(3, 3));
 
-// 2_5 LASTINDEXOF()
+// 2_5 lastIndexOf()
 // returns first index of an element starting from particular position (default last element) while searching from left to right
 // return -1 if not found
 console.log("lastindexOf() 3 in [1,2,3,4,5,3] from index -1 : ", [1,2,3,4,5,3].lastIndexOf(3));
 console.log("lastindexOf() 3 in [1,2,3,4,5,3] from index -2 : ", [1,2,3,4,5,3].lastIndexOf(3, -2));
 
-// 3_1 FILL()
+// 3_1 fill()
 // fill all or part of the array with a certain element by rewriting the original array
 // fill(value, start_index_to_fill, stop_index_to_fill)
 console.log("fill() all elements in [1,2,3,4,5] with 2 : ", [1,2,3,4,5].fill(2));
 console.log("fill() 0 from 3rd to 5th position in [1,2,3,4,5,6,7] : ", ...[1,2,3,4,5,6,7].fill(0,2,5));
 
-// 3_2 VALUEOF()
+// 3_2 valueOf()
 // get the value of array. It is the same as the original array.
 console.log("valueOf() ['a','b','c'] : ", ['a','b','c'].valueOf());
 
-// 3_3 ENTRIES()
+// 3_3 entries()
 // returns array iterator object with key/value pairs
 console.log("entries() : ")
 for (let num of empList.entries()) {
     console.log(num)
 }
 
-// 3_4 SORT()
+// 3_4 sort()
 // sort array elements in ascending order
 console.log("sort() of " + numList + " : " + numList.sort());
 // ascending using compare function
@@ -246,11 +424,11 @@ console.log("compare function descending [5,1,2,0] : " + [5,1,2,0].sort(function
 // random sorting
 console.log("random sorting [5,1,2,0,4] : " + [5,1,2,0,4].sort(function(){return 0.5 - Math.random()}));
 
-// 3_5 REVERSE()
+// 3_5 reverse()
 // reverse the elements of an array
 console.log("reverse() of [5,4,1,2] : " + [5,4,1,2].reverse());
 
-// 3_6 COPYWITHIN()
+// 3_6 copyWithin()
 // copies array elements to another position in the same array after overwriting existing values.
 // changes the current array
 // copyWithin(index_position_to_copy_the_elements, start_index-defalt0, end_index-default_arraylength)
@@ -260,13 +438,13 @@ console.log("copyWithin() (2,0) in [0,1,2,3,4,5,6,7,8,9,10] : ", ...[0,1,2,3,4,5
 // copy the elements between index 3 and 8, from position 0 of current array 
 console.log("copyWithin() (0,3,8) in [0,1,2,3,4,5,6,7,8,9,10] : ", ...[0,1,2,3,4,5,6,7,8,9,10].copyWithin(0,3,8));
 
-// 4_1 INCLUDES()
+// 4_1 includes()
 // returns true if an array includes a specific element starting from particular position (default 0)
 // includes(element) or includes(element, start_position)
 console.log("includes() 2 in [7,2,8,4] from index 0 : ", [7,2,8,4].includes(2));
 console.log("includes() 2 in [7,2,8,4] from index 2 : ", [7,2,8,4].includes(2,2));
 
-// 4_2 SOME()
+// 4_2 some()
 // returns true if any of the array element pass a condition
 // returns false if all array elements fails a condition
 function isGreater(num : any){
@@ -274,41 +452,41 @@ function isGreater(num : any){
 }
 console.log("some() > 2 in " + numList + " : " + numList.some(isGreater));
 
-// 4_3 EVERY()
+// 4_3 every()
 // returns true if all elements pass a condition
 console.log("every() > 2 in " + numList + " : " + numList.every(isGreater));
 
-// 4_4 FILTER()
+// 4_4 filter()
 // to filter the array elements based on a certain condition
 let results = numList.filter((num)=>num>2);
 console.log("filter() : " + results);
 
-// 4_5 FIND()
+// 4_5 find()
 // find a particular object 
 let emp = empList.find((emp)=>emp==='Menu');
 console.log("find() : "+ emp);
 
-// 5_1 FLAT()
+// 5_1 flat()
 // to reduce the dimensionality of an array
 console.log("flat() [[1,2],[3]] : " + [[1,2],[3]].flat());
 
-// 5_2 CONCAT()
+// 5_2 concat()
 // create a new array by merging two or more arrays
 console.log("concat() [1,2,3], [3,4,5] and [1, 2] : " + [1,2,3].concat([3,4,5], [1,2]));
 // alternatively concat can also be used to attach a new string to an exisiting string array
 console.log("concat() Peenu to " + empList + " : " + empList.concat("Peenu")); 
 
-// 5_3 JOIN()
+// 5_3 join()
 // convert array to string values separated by user defined separator
 console.log("join() : "  + empList.join(" - "));
 
-// 6_1 MAP()
+// 6_1 map()
 // creates a new array after executing a function for every array element
 // returns a new array containing a modified form of the previous array
 // eg : transform the value of each element
 console.log("map() cube root [343,8,27,64,125] : " + [343,8,27,64,125].map(Math.cbrt));
 
-// 6_2 FLATMAP()
+// 6_2 flatMap()
 // creates a new array after executing one or more functions on the entire elements of a given array
 function multiplyTable(num : any){
     return [num*1, num*2, num*3, num*4]
@@ -317,7 +495,7 @@ console.log("flatMap() multiplication table [1,2,3,4] : ", [1,2,3,4].flatMap(mul
 // Alternatively the same can be implemented via a single line function
 // console.log("flatMap() multiplication table [1,2,3,4] : ", [1,2,3,4].flatMap((x)=>[x*1,x*2,x*3,x*4]));
 
-// 6_3 FOREACH()
+// 6_3 forEach()
 // executes a function on each element in an array after iterating through the list
 // returns undefined / does not return anything
 // eg: saving/printing each element
@@ -331,43 +509,43 @@ numList.forEach((num, index, arr) => {
 });
 console.log(arr_res);
 
-// 7_1 POP()
+// 7_1 pop()
 // removes the last array element
 console.log("pop() " + numList + " : " + "to remove " + numList.pop() + " & new array " + numList);
 
-// 7_2 PUSH()
+// 7_2 push()
 // inserts an array element
 console.log("push() 2 to " + numList + " : " +  "new length " + numList.push(2) + " & new array " + numList);
 
-// 7_3 SHIFT()
+// 7_3 shift()
 // shift out the first element
 console.log("shift() : " + numList + " after shifting " + numList.shift() + " = " + numList);
 
-// 7_4 UNSHIFT()
+// 7_4 unshift()
 // adds a new element to the beginning and returns the new length
 console.log("unshift() : " + numList + " with 7 has new length " + numList.unshift(7) + " & new array " + numList);
 
-// 7_5 SLICE()
+// 7_5 slice()
 // returns a new array that contains extracted elements from an array
 // slice(start_position) or slice(start_position, end_position)
 console.log("slice() from 3rd position in " + numList + " : " + numList.slice(2));
 console.log("slice() between 2nd and 5th position in " + numList + " : " + numList.slice(2,4));
 
-// 7_6 SPLICE()
+// 7_6 splice()
 // changes the current array by adding or removing some elements
 // splice(position_to_add_elements, number_of_elements_to_be_removed, add_element1, add_element2, add_element3, ...)
 console.log("splice() add Keenu, Deenu to " + empList + ": to remove " + empList.splice(2,1,"Keenu","Deenu") + " & new array " + empList);
 // splice can also be used only to remove elements from an array
 console.log("splice remove " + empList.splice(1,2) + " & new array " + empList);
 
-// 7_7 REDUCE()
+// 7_7 reduce()
 // using reduce to perform operations on array by going from left to right
 // It returns a single value, the functions accumulated result, without changing the original array
 // no operation on empty array
 let sum_val = numList.reduce((acc,num) => acc+num); // calculate the sum until it reaches at the enc of the array 
 console.log("reduce() - sum of " + numList + " : " + sum_val);
 
-// 7_8 REDUCERIGHT()
+// 7_8 reduceRight()
 // using reduce to perform operations on array by going from right to left
 function diff(acc: any, num: any) {
     return acc - num;
@@ -377,15 +555,15 @@ console.log("reduceRight() - diff of " + numList + " : " + diff_val);
 //--------------------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------------------
-// LENGTH
+// length
 // get length of array
 console.log("Length of " + numList + " : " + numList.length);
 
-// CONSTRUCTOR
+// constructor
 // the property returns the function that created the array prototype
 console.log("constructor for [1,2,3]: " + [1,2,3].constructor); 
 
-// PROTOTYPE
+// prototype
 // allows creation of new properties and methods to arrays
 // https://stackoverflow.com/questions/12802383/extending-array-in-typescript
 // Without the export {}, you will get the TS error:
